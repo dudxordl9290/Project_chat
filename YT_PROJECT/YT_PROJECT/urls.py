74fab81c082f147d640929f9c6cfb056d6ed3587
dudxordl9290@gmail.com <kaid.gs.project@gmail.com>
2023-02-02 10:40:46 +0900

    0202-1

--- YT_PROJECT/YT_PROJECT/urls.py
+++ YT_PROJECT/YT_PROJECT/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatapp/',include('chatapp.urls')),
+    path('',include('chatapp.urls')),
 ]
