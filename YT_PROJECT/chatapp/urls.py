768751e1a52cb3b8987cdb826d0f0cdb7eb4fbdf
unknown <dudxordl9290@gmail.com>
2023-02-05 21:56:59 +0900

    home-0205-final

--- YT_PROJECT/chatapp/urls.py
+++ YT_PROJECT/chatapp/urls.py
@@ -5,7 +5,9 @@ appname = 'chatapp'
 
 urlpatterns = [
     path('',views.chat_home),
+    path('create_account/',views.create_account),
     path('join/',views.join, name='join'),
+    path('room_list/',views.room_list),
     path('make_room/',views.make_room, name='make_room/'),
     path('db_insert_room/', views.db_insert_room),
 ]
--- YT_PROJECT/chatapp/views.py
+++ YT_PROJECT/chatapp/views.py
@@ -9,13 +9,20 @@ def chat_home(request):
     context = {}
     return render(request, 'chatapp/login.html', context=context)
 
+def create_account(request):
+    context = {}
+    return render(request, 'chatapp/create_account.html', context=context)
+
 def join(request):
     context = {}
     return render(request, 'chatapp/join.html', context=context)
 
+def room_list(request):
+    room_object = Room.objects.all()
+    context = {'objects': room_object}
+    return render(request, 'chatapp/room_list.html', context=context)
 
 def make_room(request):
-   
     context = {'people_number': range(2,21), 'date_number': range(1,11)}
     return render(request, 'chatapp/make_room.html', context=context)
 
@@ -23,7 +30,7 @@ def db_insert_room(request):
      print(request)
      if request.method == 'POST':
         room_name = request.POST['room_name']
-        room_subject = request.POST['room_name']
+        room_subject = request.POST['room_subject']
         limit_people = request.POST['limit_people']
         limit_date = request.POST['limit_date']
         print(room_name, room_subject, limit_date, limit_people)
