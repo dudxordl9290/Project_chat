adc819a298e4c7f95378ae44c3b393b53917cca8
dudxordl9290@gmail.com <kaid.gs.project@gmail.com>
2023-04-19 13:40:06 +0900

    0419-re

--- YT_PROJECT/chatapp/admin.py
+++ YT_PROJECT/chatapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Room, Review
+from .models import Room, Review, ReReview
 
 # Register your models here.
 admin.site.register(Room)
 admin.site.register(Review)
+admin.site.register(ReReview)
