073a6260e1896f65456a546961156c79240daff9
dudxordl9290@gmail.com <kaid.gs.project@gmail.com>
2023-04-20 15:20:33 +0900

    20230420

--- YT_PROJECT/YT_PROJECT/urls.py
+++ YT_PROJECT/YT_PROJECT/urls.py
@@ -23,8 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chatapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += 
