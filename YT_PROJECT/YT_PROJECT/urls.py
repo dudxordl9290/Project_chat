3e9f46f999bbf27425e97f954868d950dfc79740
dudxordl9290@gmail.com <kaid.gs.project@gmail.com>
2023-04-20 15:17:07 +0900

    20230420

--- YT_PROJECT/YT_PROJECT/urls.py
+++ YT_PROJECT/YT_PROJECT/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chatapp.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT,'insecure':True}),
 ]
 
 # if settings.DEBUG:
